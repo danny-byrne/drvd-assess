d36cd7f8c2df7956d05a0966e5e7c74e89d5a0d3
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-04-13 14:35:09 -0700

    most of my logic is there, just can't get the board to render

--- components/board.js
+++ components/board.js
@@ -7,6 +7,27 @@ const Square = (props) => {
   return <div style={{height:'100', width:'100', backgroundColor:{color}}} />
 }
 
+const createBoard = function (dimensions) {
+  console.log('creating board');
+  let result = []
+  let boardDimensions = dimensions*dimensions;
+  let box = 0;
+  while(box < boardDimensions){
+    if(!box % 2){
+      result.push(<Square color="black"/>)
+    } else {
+      result.push(<Square color="white"/>)
+    }
+  }
+
+  return result.map(e => {
+    return e;
+  })
+  // return result.reduce((acc,val)=> {
+  //   return acc + val;
+  // })
+}
+
 class Board extends Component {
   constructor(){
     super()
@@ -27,26 +48,11 @@ class Board extends Component {
   render () {
 
     let widthHeight = this.state.dimensions * 100;
-    
-    const board = function () {
-      let result = []
-      let dimensions = Math.pow(this.state.dimensions, 2);
-      let box = 0;
-      while(box < dimensions){
-        if(!box % 2){
-          result.push(<Square color="black"/>)
-        } else {
-          result.push(<Square color="white"/>)
-        }
-      }
-      return result.reduce((acc,val)=> {
-        return acc + val;
-      })
-    }
+    let checkerBoard = createBoard(this.state.dimensions)
 
     return (
       <div style={{width:{widthHeight}, height:{widthHeight}, display:'flex'}}>
-      {board}
+      {checkerBoard}
       <input name="dimensions" value={this.state.dimensions} onChange={this.changeDimensions} />
       </div>
     )
